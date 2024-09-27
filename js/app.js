7fbe138451fdac6aefa8b4ec727753239fca1fdd
Carlos Meneses <osen.10112@gmail.com>
2024-09-27 00:35:41 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-return import('https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js').then(({$}) => {
-  $(function(){
-    $(".fold-table tr.view").on("click", function(){
-      if($(this).hasClass("open")) {
-        $(this).removeClass("open").next(".fold").removeClass("open");
-      } else {
-        $(".fold-table tr.view").removeClass("open").next(".fold").removeClass("open");
-        $(this).addClass("open").next(".fold").addClass("open");
-      }
-    });
+$(function(){
+  $(".fold-table tr.view").on("click", function(){
+    if($(this).hasClass("open")) {
+      $(this).removeClass("open").next(".fold").removeClass("open");
+    } else {
+      $(".fold-table tr.view").removeClass("open").next(".fold").removeClass("open");
+      $(this).addClass("open").next(".fold").addClass("open");
+    }
   });
-})
+});

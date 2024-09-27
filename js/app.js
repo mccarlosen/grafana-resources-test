a229c559712652c550e0d3bb8bf11c9d773556c8
Carlos Meneses <osen.10112@gmail.com>
2024-09-27 01:13:54 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log(document.querySelector(".fold-table"))
-  console.log(document.querySelectorAll(".fold-table tbody tr.view"));
   document.querySelectorAll(".fold-table tbody tr.view").forEach(function(row) {
       row.addEventListener("click", function() {
           if (this.classList.contains("open")) {

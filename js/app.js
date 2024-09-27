35680186949b113a48c2e5d7fd7da32e4bdbb075
Carlos Meneses <osen.10112@gmail.com>
2024-09-27 00:48:00 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
-$(function(){
-  $(".fold-table tr.view").on("click", function(){
-    if($(this).hasClass("open")) {
-      $(this).removeClass("open").next(".fold").removeClass("open");
-    } else {
-      $(".fold-table tr.view").removeClass("open").next(".fold").removeClass("open");
-      $(this).addClass("open").next(".fold").addClass("open");
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll(".fold-table tr.view").forEach(function(row) {
+      row.addEventListener("click", function() {
+          if (this.classList.contains("open")) {
+              this.classList.remove("open");
+              let nextRow = this.nextElementSibling;
+              if (nextRow && nextRow.classList.contains("fold")) {
+                  nextRow.classList.remove("open");
+              }
+          } else {
+              document.querySelectorAll(".fold-table tr.view").forEach(function(viewRow) {
+                  viewRow.classList.remove("open");
+                  let nextFold = viewRow.nextElementSibling;
+                  if (nextFold && nextFold.classList.contains("fold")) {
+                      nextFold.classList.remove("open");
+                  }
+              });
+            
+              this.classList.add("open");
+              let nextRow = this.nextElementSibling;
+              if (nextRow && nextRow.classList.contains("fold")) {
+                  nextRow.classList.add("open");
+              }
+          }
+      });
   });
 });
